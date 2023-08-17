e0aa3dfbb77ad73b6fa91508cded4bd9c7948cf8
idrissa <1mulengaidriss@gmail.com>
2023-08-17 12:57:49 +0200

    changed some things

---
+++ src/Content.js
@@ -0,0 +1,9 @@
+const content = () => {
+    return (
+        <main>
+            <p>lorem welsofinveldfv'ef v'lf v'lf dl cd</p>
+        </main>
+    )
+}
+
+export default content
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>WELCOME</h1>
+            <h2>idriss</h2>
+        </header>
+    )
+}
+
+export default Header
